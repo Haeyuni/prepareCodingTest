d5ba3fd78e62713ffbbe48db68648388dc0905ba
Ha2yuni <godbs4710@naver.com>
2025-04-14 21:08:25 +0900

    Title: 세 정수형 변수 선언, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x(7), y(23), z(30);
+
+    cout << x << " + " << y << " = " << z;
+
+    return 0;
+}
