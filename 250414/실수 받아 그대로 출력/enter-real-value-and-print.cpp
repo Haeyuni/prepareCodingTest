40b030d6efbfaf74995c6ee27d3c452c8a986a44
Ha2yuni <godbs4710@naver.com>
2025-04-14 21:35:57 +0900

    Title: 실수 받아 그대로 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n(0);
+    cin >> n;
+    cout << fixed;
+    cout.precision(2);
+
+    cout << n;
+    return 0;
+}
