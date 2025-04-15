aadada4f4fd0e6ba4b8f38542eb2c62853397054
Ha2yuni <godbs4710@naver.com>
2025-04-15 15:11:18 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250415/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int year(0), day(0), month(0);
+
+    cin >> day;
+    cin.get();
+    cin >> month;
+    cin.get();
+    cin >> year;
+
+    cout << year << "." << day << "." << month;
+    return 0;
+}
