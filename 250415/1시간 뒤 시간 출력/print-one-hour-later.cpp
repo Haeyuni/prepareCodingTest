65c5527322acd1c5b876fc3b59cd25c97d917a81
Ha2yuni <godbs4710@naver.com>
2025-04-15 15:07:31 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250415/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+     int hour(0), minute(0);
+
+     cin >> hour;
+     cin.get();
+     cin >> minute;
+     hour++;
+     cout << hour << ":" << minute;
+    return 0;
+}
