182c06f59a94a8c63660d910951da938ce259b1c
Ha2yuni <godbs4710@naver.com>
2025-04-16 17:40:38 +0900

    Title: 알파벳에 따른 평가, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250416/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char alpha(' ');
+
+    cin >> alpha;
+
+    if (alpha == 'S')
+        cout << "Superior";
+    else if (alpha == 'A')
+        cout << "Excellent";
+    else if (alpha == 'B')
+        cout << "Good";
+    else if (alpha == 'C')
+        cout << "Usually";
+    else if (alpha == 'D')
+        cout << "Effort";
+    else
+        cout << "Failure";
+    return 0;
+}
