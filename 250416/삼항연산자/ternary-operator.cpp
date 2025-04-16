8bd351f69931b19790411d9d6690377854200a14
Ha2yuni <godbs4710@naver.com>
2025-04-16 16:01:00 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250416/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int score(0);
+
+    cin >> score;
+    score == 100 ? cout << "pass" : cout << "failure";
+    return 0;
+}
