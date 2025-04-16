c453d348b8d0e42e9db63852d2c3525083fafc8e
Ha2yuni <godbs4710@naver.com>
2025-04-16 15:59:35 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250416/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a(0), b(0);
+    cin >> a >> b;
+    a > b ? cout << a : cout << b;
+    return 0;
+}
