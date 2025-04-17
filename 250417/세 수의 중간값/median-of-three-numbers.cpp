f737b9fe7ccedd805eac5d8520257b5f331a52ed
Ha2yuni <godbs4710@naver.com>
2025-04-17 20:05:21 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250417/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a(0), b(0), c(0);
+
+    cin >> a >> b >> c;
+
+    cout << (b > a && b < c);
+    return 0;
+}
