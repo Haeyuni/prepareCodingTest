fe703c3f6c1c2f75602b5a8aeb68185f7b32806e
Ha2yuni <godbs4710@naver.com>
2025-04-17 19:46:03 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250417/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a(0), b(0), c(0), d(0), e(0);
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << (a>b) << endl << (a>c) << endl << (a>d) << endl << (a>e);
+    return 0;
+}
