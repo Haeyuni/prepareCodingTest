c4a43af3c3c37afd045e0ef186949a0c7a8c3980
Ha2yuni <godbs4710@naver.com>
2025-04-17 19:53:55 +0900

    Title: 시력 검사, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250417/시력 검사/eye-test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a(0), b(0);
+
+    cin >> a >> b;
+    
+    if (a >= 1 && b >= 1)
+        cout << "High";
+    else if (a >= 0.5 && b >= 0.5)
+        cout << "Middle";
+    else
+        cout << "Low";
+
+    return 0;
+}
